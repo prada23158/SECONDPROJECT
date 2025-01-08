7b38dbf7c83b61d96f23f81a7b4d6f501f0e31b2
prada23158 <broujoseph15@gmail.com>
2025-01-08 17:54:22 +0100

    ajout de la fonction de difference

--- calcul.js
+++ calcul.js
@@ -2,3 +2,31 @@ function somme(a, b) {
     return a + b;
 }
 console.log("La somme de 1 et 2 est :", somme(1, 2));
+
+
+function difference(a, b) {
+    return Math.abs(a - b);
+}
+
+let a = 1;
+let b = 2;
+
+console.log("La soustraction de 1 et 2 est :", difference(a, b));
+
+//Fonction de produit
+function produit(a, b) {
+    return a * b;
+}
+
+console.log("Le produit entre 1 et 2 est :", produit(1, 2));
+
+// Fonction de division
+function division(a, b) {
+    if( b == 0){
+        return "Impossible de diviser par O";
+    }
+
+    return a / b;
+}
+
+console.log("La division entre 1 et 2 est :", division(1, 2));
