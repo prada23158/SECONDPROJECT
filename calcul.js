73b2e631356998f2df09266ed7586aeaf6f8f65c
prada23158 <broujoseph15@gmail.com>
2025-01-08 18:14:40 +0100

    ajout de la fonction de produit

--- calcul.js
+++ calcul.js
@@ -2,3 +2,10 @@ function somme(a, b) {
     return a + b;
 }
 console.log("La somme de 1 et 2 est :", somme(1, 2));
+
+//Fonction de produit
+function produit(a, b) {
+    return a * b;
+}
+
+console.log("Le produit entre 1 et 2 est :", produit(1, 2));
