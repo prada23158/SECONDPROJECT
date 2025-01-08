bc75c82d03d79874222e67885e5430de17f80c99
prada23158 <broujoseph15@gmail.com>
2025-01-08 18:44:40 +0100

    ajout de la fonction de division

--- calcul.js
+++ calcul.js
@@ -2,3 +2,12 @@ function somme(a, b) {
     return a + b;
 }
 console.log("La somme de 1 et 2 est :", somme(1, 2));
+
+//fonction de division
+function division(a, b) {
+    if( b == 0){
+        return "Erreur: Division par 0";
+    }
+    return a + b;
+}
+console.log("La division de 1 et 2 est :", division(1, 2));
